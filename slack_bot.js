508b97b28e27724bc9acfca9a2612ac34d46bc11
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-23 23:09:00 -0700

    make simple changes

--- slack_bot.js
+++ slack_bot.js
@@ -47,7 +47,7 @@ var listings = {
             },
             4000: {
                 moveInDate: {
-                    asap: ['https://sfbay.craigslist.org/sfc/apa/5634975188.html','https://sfbay.craigslist.org/sfc/apa/5638851010.html'],
+                    asap: ['http://sfbay.craigslist.org/sfc/apa/5640545004.html','https://sfbay.craigslist.org/sfc/apa/5638851010.html'],
                     notasap: ['no listings available right now']
                 }
             }
@@ -57,13 +57,13 @@ var listings = {
         budget: {
             3000: {
                 moveInDate: {
-                    asap: ['https://sfbay.craigslist.org/sfc/apa/5638879629.html','https://sfbay.craigslist.org/sfc/apa/5638705853.html'],
+                    asap: ['http://sfbay.craigslist.org/sfc/apa/5649653785.html','http://sfbay.craigslist.org/sfc/apa/5640230538.html'],
                     notasap: ['https://sfbay.craigslist.org/sfc/apa/5637721697.html','https://sfbay.craigslist.org/sfc/apa/5628136435.html']
                 }
             },
             4000: {
                 moveInDate: {
-                    asap: ['https://sfbay.craigslist.org/sfc/apa/5638898502.html','https://sfbay.craigslist.org/sfc/apa/5633060549.html'],
+                    asap: ['http://sfbay.craigslist.org/sfc/apa/5639519965.html','https://sfbay.craigslist.org/sfc/apa/5633060549.html'],
                     notasap: ['https://sfbay.craigslist.org/sfc/apa/5638834890.html','https://sfbay.craigslist.org/sfc/apa/5637681672.html']
                 }
             }
@@ -73,25 +73,19 @@ var listings = {
 
 controller.hears(['Hello', 'Hi','Hey'], 'direct_message,direct_mention,mention', function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
-        bot.reply(message, "Hey there - Thanks for trying out frontdoor! My name is Stew - here to handle your tedious apartment search in the city - so I'm kind of your personal assistant. Ping me whenever and I'll send you a daily dose of beautiful apartments matching your living preferences if you just tell more about when you are moving, your budget, and what neighborhood you prefer to live in. Would you like to start now?");
+        bot.reply(message, "Hello there. My name is Stew - I'm kind of your personal assistant. Moving soon? Tell me when? What neighborhood are you interested in? What's your rent price? Should we get started?");
     });
 });
 
 controller.hears(['Yes', 'yes'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
-        bot.reply(message, "Great! When are you moving? You can say 'in the next 30 days' or 'not this month' ");
+        bot.reply(message, "Great! When are you moving? You can say 'in the next 30 days' or 'not soon' ");
     });
 });
 
-controller.hears(['No', 'no','nope','Nope'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    controller.storage.users.get(message.user, function(err, user) {
-        bot.reply(message, "Sure thing! No rush! Feel free to ping me anytime by saying 'Hi' or 'Hey' ");
-    });
-});
-
-controller.hears(['in the next 30 days', 'not this month'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['in the next 30 days', 'not soon'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
         if (message.text === 'in the next 30 days') {
@@ -103,6 +97,13 @@ controller.hears(['in the next 30 days', 'not this month'], 'direct_message,dire
     });
 });
 
+controller.hears(['No', 'no','nope','Nope'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        bot.reply(message, "Sure thing! All good in the 'Hood - Hola at me anytime, just say 'Hey'");
+    });
+});
+
 controller.hears(['3000', '4000'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
@@ -111,7 +112,7 @@ controller.hears(['3000', '4000'], 'direct_message,direct_mention,mention', func
        } else {
             userAnswers['budget'] = '4000'
        }
-       bot.reply(message, "Great! Which neighborhood are you looking to move into? As of now, you can say 'glen park', 'mission' or 'SOMA' ");
+       bot.reply(message, "Great! Which 'hood are you looking into? As of now, you can say 'glen park', 'mission' or 'SOMA'");
     });
 });
 
@@ -126,13 +127,13 @@ controller.hears(['glen park', 'mission', 'SOMA'], 'direct_message,direct_mentio
              userAnswers['neighborhood'] = 'SOMA'
         }
         availableListings = listings[userAnswers['neighborhood']]['budget'][userAnswers['budget']]['moveInDate'][userAnswers['moveInDate']]
-        bot.reply(message, "Here are some listings you might be interested in: " + availableListings[0] + ' & ' + availableListings[1] );
+        bot.reply(message, "I found cool digs you might be interested in: " + availableListings[0] + ' & ' + availableListings[1] );
     });
 });
 
 controller.hears(['Thanks', 'thanks','Thank you','thank you'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
-        bot.reply(message, "Ofcourse! Always a pleasure to help!");
+        bot.reply(message, "You'd do the same for me :)");
     });
 });
